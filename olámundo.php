16b13c8a062d87c0f899141b74efd09f88119114
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-05-12 12:12:08 -0300

    Add files via upload

---
+++ olámundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//Código PHP
+  echo "<h1>Primeiro programa </h1>";
+  echo "<h1>Segundo Ano </h1>";
+  echo "<h3>Senac<h3>";
+  echo "Olá mundo!";
+?> 
+
+</body>
+</html>
