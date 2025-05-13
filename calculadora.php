a2a49b80b5714d0d00456a90117c2eb5f3b01bda
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-05-13 12:12:12 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>Calculadora</h1>";
+$a=10;
+$b=5;
+$soma = $a+$b;
+echo "<h2>Soma</h2>";
+echo "$a + $b = $soma";
+
+$subtracao=$a-$b;
+echo "<h2>Subtração</h2>";
+echo "$a-$b=$subtracao";
+$divisao=$a/$b;
+echo "<h2>Divisao</h2>";
+echo "$a /$b=$divisao";
+echo "<h2>Multiplicacao</h2>";
+$multiplicacao=$a-$b;
+echo "$a*$b=$multiplicacao";
+
+?> 
+
+</body>
+</html>
