d7a3a4b605bb05feadcf15f185ddb7b1de6904bb
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-05-13 11:41:47 -0300

    Add files via upload

---
+++ tipodedado.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x=2025;
+echo  var_dump($x);
+$nome="Senac"; 
+echo "<br>";
+echo var_dump($nome);
+$salario = 1890.00;
+echo "<br>";
+echo var_dump($salario);
+$ehestudante=true;
+echo var_dump($ehestudante);
+?> 
+
+</body>
+</html>
