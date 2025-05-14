e05add8a92badbf74c4cdd751092b153e93b79d0
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-05-14 09:09:18 -0300

    Add files via upload

---
+++ entrevista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>Projeto Integrador</h1>";
+echo "<h2>Entrevista</h2>";
+// Definição de variáveis
+$nome = "João da Silva";
+$necessidade_fisica = "Sim";   // ou "Não"
+$necessidade_auditiva = "Não"; // ou "Sim"
+$necessidade_visual = "Sim";   // ou "Não"
+// Exibição das informações
+echo "Nome: " . $nome . "<br>";
+echo "Necessidade Física: " . $necessidade_fisica . "<br>";
+echo "Necessidade Auditiva: " . $necessidade_auditiva . "<br>";
+echo "Necessidade Visual: " . $necessidade_visual . "<br>";
+?> 
+
+</body>
+</html>
