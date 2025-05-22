4c1929ecb00688ac312b9a6e3956f7e7fd136c26
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-05-22 12:20:32 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,27 +6,81 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>PET SHOP </title>
     </head>
+    <style>
+        form {
+        /* Apenas para centralizar o form na página*/
+        margin: 0 auto;
+        width: 400px;
+        /* Para ver as bordas do formulário*/
+        padding: 1em;
+        border: 1px solid#CCC;
+    }
+     form div+div {
+        margin-top: 1em;
+     }
+     label {
+        /*Para ter certeza que todas as labels tem o mesmo tamanho e estão propriamente alinhadas*/
+        display: inline-block;
+        width: 90px;
+        text-align: right;
+     }
+     input, textarea {
+        /*Para certificar que todos os campos tem as mesmas configurações de fonte. Por padrão, textareas devem ter uma fonte monospace */
+        font:1em sans-serif;
+        /*Para dar o mesmo tamanho a todos os campos de texto*/
+        width: 300px;;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        /*Para harmonizar o look & feel das bordas nos campos de texto*/
+        border: 1px solid #999
+     }
+     input:focus, textarea:focus {
+        border-color: blue;
+     }
+     textarea{
+      /*Para alinhar corretamente os campos de texto de várias linhas com sua label*/
+      vertical-align: top;
+      /*Para dar espaço suficiente para digitar algum texto*/
+      height: 5em;
+      /*Para permitir aos usuários redimensionarem qualquer textarea verticalmente.
+      Ele não funciona em todos os browsers*/
+      resize: vertical;
+     }
+	 h1 {
+		 font-family : arial;
+		 font-size: 30px;
+		 color: brown;
+	 }
+
+    </style>
     <body>
         <form method ="post">
-            <h1>Tabuada </h1>   
-            <label for ="fname">Número:</label><br>
-            <input type ="text" id ="numero" name ="numero" value =""><br>
-            <input type ="submit" value =" Enviar">
-        </form>
-     
+         <h1>Venda de Produtos</h1>  
+         <label for ="fname">Nome do Pet:</label><br>
+         <input type ="texto" id = "nome" name = "nome" value =""><br>
+         <label for ="fname">Dono:</label><br>
+         <input type ="texto" id = "dono" name = "dono" value =""><br>
+         <label for ="fname">Vacinação</label><br>
+         <input type ="texto" id = "vacina" name = "vacina" value ="20.00"><br>
+         <label for ="fname">Ração:</label><br>
+         <input type ="texto" id = "racao" name = "racao" value ="15.00"><br>
+         <input type = "submit" value ="Calcular">   
+        </form>    
         <?php
-        //Comando de Repetição
-        //Tabuada
-       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          $num = $_POST["numero"];
-          echo "<h1>Tabuada do $num</h1>";
-          for($i = 0; $i<=10;$i++){
-             $r = $i*$num;
-             echo "$i X $num = $r";
-             echo "<br>";
-           }
+        // Váriaveis
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+          $pet = $_POST ["nome"] ;
+          $dono = $_POST ["dono"] ;  
+          $vacinacao = $_POST ["vacina"] ;  
+          $racao = $_POST ["racao"] ; 
+          echo "<h1>PDV</h1>";
+          //Cálculo
+          $vlrtotal = $vacinacao+$racao;
+          //Escreva na Tela
+          echo "o valor da vacinação que é $vacinacao junto com a da ração que é $racao custa R$ $vlrtotal <br>";
+          echo "Valor Total é igual a R$ $vlrtotal"; 
         }
         ?>
     </body>
