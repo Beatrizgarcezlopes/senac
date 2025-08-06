0bffc584e17736a3075931e1492034e2d9fdc1f5
Beatrizgarcezlopes <beagarcezlopes@gmail.com>
2025-08-06 10:26:22 -0300

    Add files via upload

---
+++ Olamundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo;
+
+/**
+ *
+ * @author BEATRIZGARCEZLOPES
+ */
+public class Olamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Metodo Principal
+        String nome = "Beatriz";
+        String sobrenome = " Garcez ";
+        System.out.println("Olá Mundo !");
+        System.out.println("Bom dia " +nome);
+         System.out.println(nome+""+sobrenome);
+    }
+    
+}
